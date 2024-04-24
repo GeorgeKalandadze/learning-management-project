35946b9a350aaa3432151b01a9af1aafecfe18d5
George Kalandadze <kalandadzeg777@gmail.com>
2024-04-24 23:03:27 +0400

    feat/make component for category card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CategoryCardComponent } from '../../category-card/category-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CategoryCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
