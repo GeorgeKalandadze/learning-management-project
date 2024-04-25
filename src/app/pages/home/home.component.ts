ce5fa53eb175e4709139bd1ffb20a183e6cc64c0
George Kalandadze <kalandadzeg777@gmail.com>
2024-04-25 23:30:48 +0400

    feat/make customers comments card component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CategoryCardComponent } from '../../category-card/category-card.component';
 import { CourseCardComponent } from '../../course-card/course-card.component';
 import { InstructorCardComponent } from '../../instructor-card/instructor-card.component';
+import { CustomerCardComponent } from '../../customer-card/customer-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CategoryCardComponent, CourseCardComponent, InstructorCardComponent],
+  imports: [CategoryCardComponent, CourseCardComponent, InstructorCardComponent, CustomerCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
