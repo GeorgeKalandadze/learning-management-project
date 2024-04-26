b13f65abc49556297166f53c0539eaf0131343e4
George Kalandadze <kalandadzeg777@gmail.com>
2024-04-26 14:56:16 +0400

    feat/make footer

--- src/app/customer-card/customer-card.component.ts
+++ src/app/customer-card/customer-card.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-customer-card',
   standalone: true,
   imports: [],
   templateUrl: './customer-card.component.html',
-  styleUrl: './customer-card.component.css'
+  styleUrl: './customer-card.component.css',
 })
 export class CustomerCardComponent {
-
+  @Input() imageUrl: string = '';
+  @Input() fullName: string = '';
+  @Input() profession: string = '';
+  @Input() comment: string = '';
 }
