626ceaf9832b86aaf9f3da96eac750b3362619cb
George Kalandadze <kalandadzeg777@gmail.com>
2024-04-26 15:50:20 +0400

    refactor/modify folders and files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
-import { FooterComponent } from './footer/footer.component';
-
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { CategoryCardComponent } from '../../category-card/category-card.component';
-import { CourseCardComponent } from '../../course-card/course-card.component';
-import { InstructorCardComponent } from '../../instructor-card/instructor-card.component';
-import { CustomerCardComponent } from '../../customer-card/customer-card.component';
+import { CategoryCardComponent } from '../../components/category-card/category-card.component';
+import { CourseCardComponent } from '../../components/course-card/course-card.component';
+import { InstructorCardComponent } from '../../components/instructor-card/instructor-card.component';
+import { CustomerCardComponent } from '../../components/customer-card/customer-card.component';
 
 @Component({
   selector: 'app-home',
