68ea9adc20f032a21c0e7a533933ea364c415147
George Kalandadze <kalandadzeg777@gmail.com>
2024-04-27 18:28:26 +0400

    feat/make sections abou mentor on single course page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CourseComponent } from './pages/course/course.component';
+import { SingleCourseComponent } from './pages/single-course/single-course.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'courses', component: CourseComponent },
+  { path: 'single-course', component: SingleCourseComponent },
 ];
