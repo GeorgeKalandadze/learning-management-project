b88ad2196ba84b3e8566a881ae8f2a054b116b88
George Kalandadze <kalandadzeg777@gmail.com>
2024-04-27 19:47:25 +0400

    feat/add customer comments and courses section on single course page

--- src/app/pages/single-course/single-course.component.ts
+++ src/app/pages/single-course/single-course.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { LearningManagementService } from '../../services/learning-management.service';
+import { CustomerCardComponent } from '../../components/customer-card/customer-card.component';
+import { NgFor } from '@angular/common';
+import { CourseCardComponent } from '../../components/course-card/course-card.component';
 
 @Component({
   selector: 'app-single-course',
   standalone: true,
-  imports: [],
+  imports: [CourseCardComponent, CustomerCardComponent, NgFor],
   templateUrl: './single-course.component.html',
-  styleUrl: './single-course.component.css'
+  styleUrl: './single-course.component.css',
 })
 export class SingleCourseComponent {
+  comments: any[] = [];
+  courses: any[] = [];
 
+  constructor(private learningManagementService: LearningManagementService) {}
+
+  ngOnInit(): void {
+    this.comments = this.learningManagementService.getComments();
+    this.courses = this.learningManagementService.getCourses();
+  }
 }
