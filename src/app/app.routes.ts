d7d5a807947150fad8aaa1d02887242cf86f3fc3
George Kalandadze <kalandadzeg777@gmail.com>
2024-05-05 00:25:26 +0400

    feat/make cart page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CourseComponent } from './pages/course/course.component';
 import { SingleCourseComponent } from './pages/single-course/single-course.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'courses', component: CourseComponent },
   { path: 'course/:id', component: SingleCourseComponent },
+  { path: 'cart', component: CartComponent },
 ];
