2856c7c490376c47944d0c7e6b543af77cb142b5
George Kalandadze <kalandadzeg777@gmail.com>
2024-05-05 01:09:30 +0400

    feat/make implementation of service in cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../services/cart/cart.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrl: './cart.component.css',
 })
 export class CartComponent {
+  cartItems: any[] = [];
+  totalAmount: number = 0;
 
+  constructor(private cartService: CartService) {
+    this.cartItems = this.cartService.getCartItems();
+  }
+
+  removeFromCart(courseId: number) {
+    this.cartService.removeFromCart(courseId);
+    this.cartItems = this.cartService.getCartItems();
+  }
+
+  increaseQuantity(courseId: number) {
+    this.cartService.increaseQuantity(courseId);
+    this.cartItems = this.cartService.getCartItems();
+  }
+
+  decreaseQuantity(courseId: number) {
+    this.cartService.decreaseQuantity(courseId);
+    this.cartItems = this.cartService.getCartItems();
+  }
+
+  getTotalAmount(): number {
+    let totalAmount = 0;
+    for (const item of this.cartItems) {
+      totalAmount += item.price * item.quantity;
+    }
+
+    return totalAmount;
+  }
 }
--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -4,28 +4,33 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CartService {
+  private cartItemsKey = 'cartItems';
   private cartItems: any[] = [];
 
-  constructor() {}
+  constructor() {
+    const storedCartItems = sessionStorage.getItem(this.cartItemsKey);
+    if (storedCartItems) {
+      this.cartItems = JSON.parse(storedCartItems);
+    }
+  }
 
   addToCart(course: any) {
     const existingItemIndex = this.cartItems.findIndex(
       (item) => item.id === course.id
     );
     if (existingItemIndex !== -1) {
-      console.log('Course already exists in the cart.');
       return;
     }
     course.quantity = 1;
     this.cartItems.push(course);
-    console.log('Course added to cart:', course);
-    console.log('Current cart items:', this.cartItems);
+    this.saveCartItemsToStorage();
   }
 
   removeFromCart(courseId: number) {
     const index = this.cartItems.findIndex((item) => item.id === courseId);
     if (index !== -1) {
       this.cartItems.splice(index, 1);
+      this.saveCartItemsToStorage();
     }
   }
 
@@ -33,17 +38,38 @@ export class CartService {
     const item = this.cartItems.find((item) => item.id === courseId);
     if (item) {
       item.quantity++;
+      this.saveCartItemsToStorage();
     }
   }
 
   decreaseQuantity(courseId: number) {
-    const item = this.cartItems.find((item) => item.id === courseId);
-    if (item && item.quantity > 1) {
-      item.quantity--;
+    const itemIndex = this.cartItems.findIndex((item) => item.id === courseId);
+    if (itemIndex !== -1) {
+      if (this.cartItems[itemIndex].quantity > 1) {
+        this.cartItems[itemIndex].quantity--;
+      } else {
+        this.cartItems.splice(itemIndex, 1);
+      }
+      this.saveCartItemsToStorage();
     }
   }
 
   getCartItems(): any[] {
     return this.cartItems;
   }
+
+  getTotalAmount(): number {
+    let totalAmount = 0;
+    for (const item of this.cartItems) {
+      totalAmount += item.price * item.quantity;
+    }
+    
+    return totalAmount;
+  }
+
+  private saveCartItemsToStorage() {
+    sessionStorage.setItem(this.cartItemsKey, JSON.stringify(this.cartItems));
+  }
+
+  
 }
